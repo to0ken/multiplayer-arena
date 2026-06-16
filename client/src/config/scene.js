ab8d98d1e75df89feab19db5e354004ed5236cc5
to0ken <evgeniamafinova@gmail.com>
2026-06-16 23:14:16 +0300

    Create scene.js

---
+++ client/src/config/scene.js
@@ -0,0 +1,17 @@
+export const SCENE_CONFIG = {
+  arenaSize: 20,
+  playerSpeed: 0.15,
+  playerRadius: 0.5,
+  colors: {
+    background: 0x1a1a2e,
+    grid: 0x4ecdc4,
+    floor: 0x16213e
+  },
+  camera: {
+    fov: 60,
+    near: 0.1,
+    far: 1000,
+    position: { x: 0, y: 15, z: 15 },
+    lookAt: { x: 0, y: 0, z: 0 }
+  }
+};
